55a5ea9ccc2b2eb3c4d7f623bd8298ca89ef2c10
PJC-1 <jumpzine@gmail.com>
2019-04-01 14:01:22 -0700

    added example to consolidate and display 2 lists of status enums

--- import_IModuleManager.java
+++
@@ -1,9 +0,0 @@
-import com.polarion.alm.tracker.ITrackerService;
-import javax.servlet.http.HttpServlet;
-
-public class ImportIModuleManager<IContextId> extends HttpServlet {
-
-    ITrackerService trackerService = (ITrackerService) PlatformContext.getPlatform().lookupService(ITrackerService.class);
-    IModuleManager moduleManager = trackerService.getModuleManager();
-
-}
